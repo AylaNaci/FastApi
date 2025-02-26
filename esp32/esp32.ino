da2bffab3fb47e523bf83e59659feaddd2001e8f
AylaNaci <tahtha995@gmail.com>
2025-02-26 11:45:20 +0300

    rastgele sayı

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h> // WiFi kütüphanesini dahil et
 #include <HTTPClient.h> // HTTP istemci kütüphanesini dahil et
+#include <ArduinoJson.h> // JSON kütüphanesini dahil et
 
 const char* ssid = "calisma_salonu2"; // WiFi ağ adı (SSID)
 const char* password = "12345678"; // WiFi şifresi
@@ -14,21 +15,29 @@ void setup() {
   }
 
   Serial.println("Connected to WiFi"); // WiFi'ye bağlanıldı mesajı yazdır
-  sendRequest(); // HTTP isteği gönder
+  sendRandomNumber(); // Rastgele sayı gönder
 }
 
-void sendRequest() {
+void sendRandomNumber() {
   if (WiFi.status() == WL_CONNECTED) { // WiFi'ye bağlıysa
     HTTPClient http; // HTTP istemci nesnesi oluştur
-    http.begin("http://192.168.1.130:8000/kayit"); // HTTP isteği için URL belirle
-    int httpResponseCode = http.GET(); // GET isteği gönder ve yanıt kodunu al
+    http.begin("http://192.168.1.130:8000/random"); // HTTP isteği için URL belirle
+    http.addHeader("Content-Type", "application/json"); // JSON içeriği için başlık ekle
+
+    int randomNumber = random(0, 100); // 0 ile 100 arasında rastgele sayı üret
+    StaticJsonDocument<200> doc;
+    doc["number"] = randomNumber;
+    String requestBody;
+    serializeJson(doc, requestBody);
+
+    int httpResponseCode = http.POST(requestBody); // POST isteği gönder ve yanıt kodunu al
 
     if (httpResponseCode > 0) { // Yanıt kodu pozitifse (başarılı)
       String response = http.getString(); // Yanıtı string olarak al
       Serial.println(httpResponseCode); // Yanıt kodunu yazdır
       Serial.println(response); // Yanıt içeriğini yazdır
     } else {
-      Serial.print("Error on sending GET request: "); // GET isteği gönderme hatası mesajı yazdır
+      Serial.print("Error on sending POST request: "); // POST isteği gönderme hatası mesajı yazdır
       Serial.println(httpResponseCode); // Hata kodunu yazdır
     }
 
--- python/main.py
+++ python/main.py
@@ -11,6 +11,15 @@ async def read_root(request: Request):
         log_file.write(f"{datetime.datetime.now()}: {client_host}\n")
     return {"ip": "Kayıt tamam"}
 
+# Yeni rota: http://192.168.1.130:8000/random
+@app.post("/random")
+async def receive_random_number(request: Request):
+    data = await request.json()
+    random_number = data.get("number")
+    with open("random_numbers.txt", "a") as log_file:
+        log_file.write(f"{datetime.datetime.now()}: {random_number}\n")
+    return {"status": "success", "number": random_number}
+
 # http://192.168.1.130:8000/
 @app.get("/")
 async def read_root():
