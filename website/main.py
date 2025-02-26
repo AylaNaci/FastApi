bc2e067182f0aa38a8e456d851d1c98cf7757c0f
AylaNaci <tahtha995@gmail.com>
2025-02-26 14:40:45 +0300

    Website

---
+++ website/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import datetime
+
+app = FastAPI()
+templates = Jinja2Templates(directory="static")
+
+# http://192.168.1.130:8000/
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+# http://192.168.1.130:8000/site
+@app.get("/site", response_class=HTMLResponse)
+async def read_site(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# http://localhost:8000/
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
