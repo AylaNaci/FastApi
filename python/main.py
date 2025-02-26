839e69e315ca546dca6d086334a6ea8820d2cefa
AylaNaci <tahtha995@gmail.com>
2025-02-26 10:36:46 +0300

    ana route

--- python/main.py
+++ python/main.py
@@ -1,1 +1,12 @@
-Merhaba dünya
+from fastapi import FastAPI, Request
+import datetime
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
